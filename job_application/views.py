a6dcf2513c447a5120cd6bb3e8264cfda9a0c510
Waris Amin <warisamin@gmail.com>
2013-12-05 14:06:29 +0530

    Fixed typo

--- job_application/views.py
+++ job_application/views.py
@@ -4,11 +4,11 @@ import json
 import urllib2
 import requests
 
-client_id = '666'
+client_id = '121'
 redirect_uri = 'http://127.0.0.1:8000/job/register'
 
 def job_register(request):
-    url = '''http://10.42.0.31:8000/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=666&code=%s''' % (client_id, redirect_uri,request.GET['code'])
+    url = '''http://10.42.0.4:8001/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=121&code=%s''' % (client_id, redirect_uri,request.GET['code'])
     response = requests.get(url)
     json_data =  response.json()
     resume = open('/home/waris/Desktop/examform.pdf', 'rb')
@@ -19,9 +19,10 @@ def job_register(request):
 
                 }
     files = { 'resume' : ('resume.pdf', resume)}
-    url = 'http://10.42.0.31:8000/api/resume/upload/?access_token=%s' % json_data['access_token']
-    return requests.post(url, data=data, files = files)
-
+    url = 'http://10.42.0.4:8001/api/resume/upload/?access_token=%s' % json_data['access_token']
+    requests.post(url, data=data, files = files)
+    return HttpResponseRedirect('http://10.42.0.4:8001/accounts/profile')
+    
 def authorize(request):
-    url = 'http://10.42.0.31:8000/oauth/authorize?client_id=%s&redirect_uri=%s' % (client_id, redirect_uri)
+    url = 'http://10.42.0.4:8001/oauth/authorize?client_id=%s&redirect_uri=%s' % (client_id, redirect_uri)
     return redirect(url)
